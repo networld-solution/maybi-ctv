133c983a077db4a32bf10b1706f45910168453f6
teoit <teoit468@gmail.com>
2026-08-04 17:13:36 +0700

    js

---
+++ js/main.js
@@ -0,0 +1,91 @@
+const AppMain = (function(){
+    const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw9TgRdHzCVF31X1bg4ogwyofKxK0fZ6ClamIJTwYtAopQ3vbFOEwKDI3Kf--xJ9-J2/exec";
+
+    const frmSubmit = document.querySelector("#maybi-registration-form");
+    const submitBtn = document.querySelector("#submit-btn");
+
+    frmSubmit.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        submitBtn.disabled = true;
+        submitBtn.innerText = "Đang gửi...";
+
+        try {
+            const formData = new FormData(frmSubmit);
+
+            const data = {};
+
+            formData.forEach((value, key) => {
+                data[key] = value;
+            });
+
+            // Lấy UTM
+            const params = new URLSearchParams(window.location.search);
+
+            data.utm_source = params.get("utm_source") || "";
+            data.utm_medium = params.get("utm_medium") || "";
+            data.utm_campaign = params.get("utm_campaign") || "";
+            data.utm_term = params.get("utm_term") || "";
+            data.utm_content = params.get("utm_content") || "";
+
+            data.page = window.location.href;
+            data.user_agent = navigator.userAgent;
+            data.created_at = formatDateTime();
+
+            console.log(data);
+
+            await requestRegister(data);
+
+            alert("Đăng ký thành công");
+
+            frmSubmit.reset();
+
+        } catch (err) {
+            console.error(err);
+            alert("Có lỗi xảy ra.");
+        } finally {
+            submitBtn.disabled = false;
+            submitBtn.innerText = "ĐĂNG KÝ NGAY";
+        }
+    });
+
+    return {
+        init: function(){            
+        },
+    }
+})();
+
+async function requestRegister(data) {
+    const formData = new FormData();
+    Object.keys(data).forEach((key) => {
+        formData.append(key, data[key]);
+    });
+
+    const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbw9TgRdHzCVF31X1bg4ogwyofKxK0fZ6ClamIJTwYtAopQ3vbFOEwKDI3Kf--xJ9-J2/exec",
+        {
+            method: "POST",           
+            body: formData,
+        }
+    );
+
+    return response;
+}
+function formatDateTime(date = new Date()) {
+    const pad = (n) => String(n).padStart(2, "0");
+
+    return [
+        pad(date.getDate()),
+        pad(date.getMonth() + 1),
+        date.getFullYear()
+    ].join("-") + " " +
+    [
+        pad(date.getHours()),
+        pad(date.getMinutes()),
+        pad(date.getSeconds())
+    ].join(":");
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    AppMain.init();
+});
