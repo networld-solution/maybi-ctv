7d07ce95dad38b0b1ef20b0e2028de3d28a2966f
teoit <teoit468@gmail.com>
2026-08-04 23:05:29 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -34,11 +34,15 @@ const AppMain = (function(){
 
             console.log(data);
 
-            await requestRegister(data,"maybi");
+            const response = await requestRegister(data,"maybi");
 
-            alert("Đăng ký thành công");
-
-            frmSubmit.reset();
+            if (response && response.result === "success") {
+                alert("Đăng ký thành công");
+                frmSubmit.reset();
+            } else {
+                console.error("Lỗi từ Google Script:", response);
+                alert("Đăng ký thất bại, vui lòng thử lại.");
+            }
 
         } catch (err) {
             console.error(err);
@@ -65,10 +69,8 @@ async function requestRegister(dataForm, type) {
 
   return fetch(url, {
     method: "POST",
-    headers: {
-      "Content-Type": "text/plain",
-    },
     body: payload,
+    // Google Apps Script sẽ tự động parse body nếu không có Content-Type header
   })
     .then((response) => response.json())
     .then((data) => data);
